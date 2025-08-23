795d51b83cb81c850a334d806039f817e6213e2c
pierrotdx <pierrotdx@gmail.com>
2025-08-23 15:26:17 +0200

    21.Se passer du fichier XML

--- invoise/core/src/main/java/com/mycompany/invoise/App.java
+++ invoise/core/src/main/java/com/mycompany/invoise/App.java
@@ -3,9 +3,9 @@ package com.mycompany.invoise;
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Scanner;
 
 /**
  * Hello world!
@@ -15,7 +15,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
         invoiceController.createInvoice();
     }

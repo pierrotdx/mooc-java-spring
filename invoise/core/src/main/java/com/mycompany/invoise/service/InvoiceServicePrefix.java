5a03e1b7c94e70c52a97d9db97ab6bb64f5c1dc1
pierrotdx <pierrotdx@gmail.com>
2025-08-23 12:26:56 +0200

    16. Configuration par annotation

--- invoise/core/src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
+++ invoise/core/src/main/java/com/mycompany/invoise/service/InvoiceServicePrefix.java
@@ -2,11 +2,13 @@ package com.mycompany.invoise.service;
 
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class InvoiceServicePrefix implements InvoiceServiceInterface {
     private Long lastNumber;
     private String prefix;
 
+    @Autowired
     private InvoiceRepositoryInterface invoiceRepository;
 
     public InvoiceRepositoryInterface getInvoiceRepository() {

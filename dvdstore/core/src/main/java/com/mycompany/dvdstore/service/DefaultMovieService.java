165049fad28031462d9c3b165e54e3e55a105956
pierrotdx <pierrotdx@gmail.com>
2025-08-23 12:31:37 +0200

    exercice 9

--- dvdstore/core/src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
+++ dvdstore/core/src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
@@ -2,8 +2,10 @@ package com.mycompany.dvdstore.service;
 
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DefaultMovieService implements MovieServiceInterface {
+    @Autowired
     private MovieRepositoryInterface movieRepository;
 
     public MovieRepositoryInterface getMovieRepository() {

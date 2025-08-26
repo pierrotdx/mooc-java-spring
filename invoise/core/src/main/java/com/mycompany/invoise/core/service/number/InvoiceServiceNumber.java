86a63a0e97c59b38749969506ddacc1d520a1505
pierrotdx <pierrotdx@gmail.com>
2025-08-26 13:25:03 +0200

    47. Spring JDBC : premiere connexion

--- invoise/core/src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
+++ invoise/core/src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
@@ -3,10 +3,11 @@ package com.mycompany.invoise.core.service.number;
 import com.mycompany.invoise.core.entity.Invoice;
 import com.mycompany.invoise.core.repository.InvoiceRepositoryInterface;
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Service
+@Service
 public class InvoiceServiceNumber implements InvoiceServiceInterface {
     private static Long lastNumber = 0l;
 
--- invoise/core/src/main/java/com/mycompany/invoise/core/service/prefix/InvoiceServicePrefix.java
+++ invoise/core/src/main/java/com/mycompany/invoise/core/service/prefix/InvoiceServicePrefix.java
@@ -1,59 +1,59 @@
-package com.mycompany.invoise.core.service.prefix;
-
-import com.mycompany.invoise.core.entity.Invoice;
-import com.mycompany.invoise.core.repository.InvoiceRepositoryInterface;
-import com.mycompany.invoise.core.service.InvoiceServiceInterface;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class InvoiceServicePrefix implements InvoiceServiceInterface {
-    @Value("${invoice.lastNumber}")
-    private Long lastNumber;
-    @Value("${invoice.prefix}")
-    private String prefix;
-
-    public InvoiceServicePrefix(InvoiceRepositoryInterface invoiceRepository) {
-        this.invoiceRepository = invoiceRepository;
-    }
-
-    private final InvoiceRepositoryInterface invoiceRepository;
-
-    public InvoiceRepositoryInterface getInvoiceRepository() {
-        return invoiceRepository;
-    }
-
-    public Invoice createInvoice(Invoice invoice) {
-        invoice.setNumber(String.valueOf(this.prefix + (++lastNumber)));
-        this.invoiceRepository.create(invoice);
-        return invoice;
-    }
-
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
-    }
-
-    public Long getLastNumber() {
-        return lastNumber;
-    }
-
-    public void setLastNumber(Long lastNumber) {
-        this.lastNumber = lastNumber;
-    }
-
-    @Override
-    public List<Invoice> getInvoiceList() {
-        return this.invoiceRepository.list();
-    }
-
-    @Override
-    public Invoice getInvoiceByNumber(String number) {
-        return this.invoiceRepository.getById(number);
-    }
-}
+//package com.mycompany.invoise.core.service.prefix;
+//
+//import com.mycompany.invoise.core.entity.Invoice;
+//import com.mycompany.invoise.core.repository.InvoiceRepositoryInterface;
+//import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class InvoiceServicePrefix implements InvoiceServiceInterface {
+//    @Value("${invoice.lastNumber}")
+//    private Long lastNumber;
+//    @Value("${invoice.prefix}")
+//    private String prefix;
+//
+//    public InvoiceServicePrefix(InvoiceRepositoryInterface invoiceRepository) {
+//        this.invoiceRepository = invoiceRepository;
+//    }
+//
+//    private final InvoiceRepositoryInterface invoiceRepository;
+//
+//    public InvoiceRepositoryInterface getInvoiceRepository() {
+//        return invoiceRepository;
+//    }
+//
+//    public Invoice createInvoice(Invoice invoice) {
+//        invoice.setNumber(String.valueOf(this.prefix + (++lastNumber)));
+//        this.invoiceRepository.create(invoice);
+//        return invoice;
+//    }
+//
+//    public String getPrefix() {
+//        return prefix;
+//    }
+//
+//    public void setPrefix(String prefix) {
+//        this.prefix = prefix;
+//    }
+//
+//    public Long getLastNumber() {
+//        return lastNumber;
+//    }
+//
+//    public void setLastNumber(Long lastNumber) {
+//        this.lastNumber = lastNumber;
+//    }
+//
+//    @Override
+//    public List<Invoice> getInvoiceList() {
+//        return this.invoiceRepository.list();
+//    }
+//
+//    @Override
+//    public Invoice getInvoiceByNumber(String number) {
+//        return this.invoiceRepository.getById(number);
+//    }
+//}

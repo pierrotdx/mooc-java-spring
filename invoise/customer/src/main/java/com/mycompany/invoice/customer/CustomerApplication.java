857eba52e1fa153ae4c810cc8bf0deb0c81c8aae
pierrotdx <pierrotdx@gmail.com>
2025-08-27 16:18:26 +0200

    61. Decoupage en microservices

---
+++ invoise/customer/src/main/java/com/mycompany/invoice/customer/CustomerApplication.java
@@ -0,0 +1,19 @@
+package com.mycompany.invoice.customer;
+
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+    @Bean
+    public Hibernate6Module datatypeHibernateModule() {
+        return new Hibernate6Module();
+    }
+}
---
+++ invoise/product/src/main/java/com/mycompany/invoice/product/ProductApplication.java
@@ -0,0 +1,19 @@
+package com.mycompany.invoice.product;
+
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+    @Bean
+    public Hibernate6Module datatypeHibernateModule() {
+        return new Hibernate6Module();
+    }
+}

ac8e6d686bcfc15c0ea4ad1295bfe497b45c2032
pierrotdx <pierrotdx@gmail.com>
2025-08-27 14:57:13 +0200

    exercice 35

--- dvdstore/core/src/main/java/com/mycompany/dvdstore/core/repository/MovieRepositoryInterface.java
+++ dvdstore/core/src/main/java/com/mycompany/dvdstore/core/repository/MovieRepositoryInterface.java
@@ -1,10 +1,17 @@
 package com.mycompany.dvdstore.core.repository;
 
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import com.mycompany.dvdstore.core.entity.Movie;
 
+import java.util.Optional;
+
 public interface MovieRepositoryInterface extends CrudRepository<Movie, Long> {
-//    Movie add(Movie movie);
+    @EntityGraph(value = "movie.actor-and-reviews", type = EntityGraph.EntityGraphType.FETCH)
+    @Override
+    Optional<Movie> findById(Long aLong);
+    //    Movie add(Movie movie);
 //    List<Movie> list();
 //    Movie getById(long id);
 }
